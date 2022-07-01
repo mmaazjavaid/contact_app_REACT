c019ca121becc36f880ee9d2f76a36cf251786de
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-07-01 11:09:43 +0500

    contact card created seperately

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function ContactCard() {
+function ContactCard({name,email}) {
   return (
-    <div>ContactCard</div>
+    <div className='item'>
+          <div className='content'>
+            <div className='header'>{name}</div>
+            <div>{email}</div>
+          </div>
+          <i className='trash alternate outline icon' style={{color:"red" , marginTop:"7px" }}></i>
+        </div>
   )
 }
 
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
+import ContactCard from './ContactCard'
 
 function ContactList({contacts}) {
   return (
     <div className='ui celled list'>
      {contacts.map((contact)=>{
       return(
-        <div className='item'>
-          <div className='content'>
-            <div className='header'>{contact.name}</div>
-            <div>{contact.email}</div>
-          </div>
-          <i className='trash alternate outline icon'></i>
-        </div>
         
+        <ContactCard name={contact.name} email={contact.email} />
       )
      })}
 
